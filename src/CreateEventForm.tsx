2b5e1bbd02618a1dd39f804205ac10a8e4574e05
RunOrFight <dragoinfury@gmail.com>
2023-11-23 23:23:05 +0300

    refactor: remove ts errors

--- src/CreateEventForm.tsx
+++ src/CreateEventForm.tsx
@@ -15,7 +15,7 @@ const CreateEventForm = () => {
             id: uuid()
         }
     })
-    const onSubmit = (data) => Telegram.WebApp.sendData(JSON.stringify(data))
+    const onSubmit = (data: object) => Telegram.WebApp.sendData(JSON.stringify(data))
 
     return <form onSubmit={handleSubmit(onSubmit)} className={"h-full flex flex-col gap-2 p-4"}>
         <div className={"flex flex-col gap-2 flex-1"}>
